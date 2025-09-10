ba4a60aabb7184e8db4d5d0b218a800fddfbf0eb
Isaac Lee <isaac.wonha.lee@outlook.com>
2025-09-10 14:40:40 -0400

    feat: vercel structure

---
+++ api/index.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import os
+import sys
+
+# Add the parent directory to sys.path to import our modules
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import mongoDB
+
+# Init Code: Starts up flask app and sets up database
+app = Flask(__name__, static_url_path='')
+cors = CORS(app)
+
+us_state_abbrev = {
+    'AL': 'Alabama',        'AK': 'Alaska',        'AZ': 'Arizona',
+    'AR': 'Arkansas',       'CA': 'California',    'CO': 'Colorado',
+    'CT': 'Connecticut',    'DE': 'Delaware',      'FL': 'Florida',
+    'GA': 'Georgia',        'HI': 'Hawaii',        'ID': 'Idaho',
+    'IL': 'Illinois',       'IN': 'Indiana',       'IA': 'Iowa',
+    'KS': 'Kansas',         'KY': 'Kentucky',      'LA': 'Louisiana',
+    'ME': 'Maine',          'MD': 'Maryland',      'MA': 'Massachusetts',
+    'MI': 'Michigan',       'MN': 'Minnesota',     'MS': 'Mississippi',
+    'MO': 'Missouri',       'MT': 'Montana',       'NE': 'Nebraska',
+    'NV': 'Nevada',         'NH': 'New Hampshire', 'NJ': 'New Jersey',
+    'NM': 'New Mexico',     'NY': 'New York',      'NC': 'North Carolina',
+    'ND': 'North Dakota',   'OH': 'Ohio',          'OK': 'Oklahoma',
+    'OR': 'Oregon',         'PA': 'Pennsylvania',  'RI': 'Rhode Island',
+    'SC': 'South Carolina', 'SD': 'South Dakota',  'TN': 'Tennessee',
+    'TX': 'Texas',          'UT': 'Utah',          'VT': 'Vermont',
+    'VA': 'Virginia',       'WA': 'Washington',    'WV': 'West Virginia',
+    'WI': 'Wisconsin',      'WY': 'Wyoming',
+}
+
+# Initialize database on startup
+mongoDB.initDB(us_state_abbrev)
+
+@app.route('/', methods=['GET'])
+def test():
+    '''Developer help function of sorts.'''
+    doc = "<h1>API:</h1> <p>GET: /api/stats/state/&lt;string:state_abbrev&gt;</p>"
+    doc += "<blockquote> returns relevant statistics given a two-letter state abbreviation</blockquote>"
+    return doc
+
+@app.route('/stats/state/<string:state_abbrev>', methods=['GET'])
+def statistics(state_abbrev):
+    '''
+    This function interfaces with database code to obtain data
+    for a valid state and returns the result to the user.
+    '''
+    
+    state = state_abbrev.upper()
+    # initialize dictionary
+    if state not in us_state_abbrev:
+        data = dict()
+        data["error"] = "No such state"
+        return jsonify(data)
+
+    data = dict(stateName = us_state_abbrev[state])
+    data["totalPoliceKillings"] = mongoDB.queryDB(state)
+    data["percentKillingsBlack"] = mongoDB.getPercentKillingsBlack(state)
+    data["percentKillingsNotBlack"] = mongoDB.getPercentKillingsNotBlack(state)
+    data["percentPopulationBlack"] = mongoDB.getPercentPopulationBlack(state)
+    data["percentPopulationNotBlack"] = mongoDB.getPercentPopulationNotBlack(state)
+    data["blackDisparity"] = mongoDB.getBlackDisparity(state)
+    data["notBlackDisparity"] = mongoDB.getNotBlackDisparity(state)
+    data["totalDisparity"] = mongoDB.getTotalDisparity(state)
+
+    return jsonify(data)
+
+# Export the app for Vercel
+def handler(request):
+    return app(request.environ, lambda status, headers: None)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ public/static/main.js
@@ -0,0 +1,47 @@
+/* Description of functionality:
+ *
+ * The map of the United States is represented as an SVG (scalable vector graphics) image,
+ * which is a format that represents pictures in XML format. Our map SVG is sourced from
+ * wikipedia. The states in the image are designated with <path> elements.
+ *
+ * JQuery is used to bind click events to each <path> (state). When a state is clicked,
+ * we get the <path> id and request the server for relevant data using our RESTful API.
+ *
+ * The JSON data is reformatted into a simple list and written to <div id="stats">
+ */
+
+google.charts.load('current', {'packages':['corechart']});
+
+$("path:not(#frames)").click(function(e) {
+  let state = $(this).attr("id");
+  // Updated for Vercel deployment - uses /api/ endpoint
+  $.getJSON("/api/stats/state/" + state, function(data) {
+
+    var chartdata1 = google.visualization.arrayToDataTable([
+        ['Race', 'Percent of Population'],
+        ['Black', parseInt(data.percentPopulationBlack)],
+        ['Non-Black', parseInt(data.percentPopulationNotBlack)]
+    ]);
+    var chartdata2 = google.visualization.arrayToDataTable([
+        ['Race', 'Percent of Killings'],
+        ['Black', parseFloat(data.percentKillingsBlack)],
+        ['Non-Black', parseFloat(data.percentKillingsNotBlack)]
+    ]);
+
+    var chartoptions1 = {'title': 'Demographics of '+ data.stateName, 'width': 350, 'height': 350, chartArea:{top:50, left:20, bottom: 50, width:"100%", height:"100%"}, legend:{position: 'bottom'}};
+    var chartoptions2 = {'title': 'Police Killings in ' + data.stateName, 'width': 350, 'height': 350, chartArea:{top:50, left: 20, bottom: 50, width:"100%", height:"100%"}, legend:{position: 'bottom'}};
+
+    var chart1 = new google.visualization.PieChart(document.getElementById('chartContainer1'));
+    chart1.draw(chartdata1, chartoptions1);
+    var chart2 = new google.visualization.PieChart(document.getElementById('chartContainer2'));
+    chart2.draw(chartdata2, chartoptions2);
+
+    let stats = []
+    //$.each(data, function(key,val) {
+    //  stats.push("<li id'" + key + "'>" + key + ": " + val + "</li>");
+    //});
+    stats.push(data.totalPoliceKillings)
+    $('#stats').html(stats.join(""));
+
+  });
+});
